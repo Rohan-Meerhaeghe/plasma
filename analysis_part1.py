ddaf2ab8223af41898c1111b0f89af5f17311977
Rohan <rohan.meerhaeghe@ugent.be>
2023-11-14 15:56:02 +0100

    update analysis_part1.py

--- analysis_part1.py
+++ analysis_part1.py
@@ -11,31 +11,23 @@ def read_csv(name):
 plasma_028mbar = read_csv("plasma 0.28mbar.csv")
 plasma_0053mbar = read_csv("plasma 0.053mbar.csv")
 
-fig, ax1 = plt.subplots() 
-ax1.set_xlabel('Current [mA]') 
-ax1.set_ylabel('System voltage [V]', color = 'red') 
-ax1.plot(plasma_0053mbar[0], plasma_0053mbar[1], color = 'red') 
-ax1.tick_params(axis ='y', labelcolor = 'red') 
-  
-# Adding Twin Axes
-ax2 = ax1.twinx()
-ax2.set_ylabel('Voltage between probes [V]', color = 'blue') 
-ax2.plot(plasma_0053mbar[0], plasma_0053mbar[2], color = 'blue') 
-ax2.tick_params(axis ='y', labelcolor = 'blue')
-plt.savefig("plasma 0.0053mbar.png")
-plt.close()
+V_plasma_028mbar = plasma_028mbar[1] - plasma_028mbar[0] * 100
+# current in mA and resistor in kOhm, so the milli and kilo cancel.
+V_plasma_0053mbar = plasma_0053mbar[1] - plasma_0053mbar[0] * 100
 
 
-fig, ax1 = plt.subplots() 
-ax1.set_xlabel('Current [mA]') 
-ax1.set_ylabel('System voltage [V]', color = 'red') 
-ax1.plot(plasma_028mbar[0], plasma_028mbar[1], color = 'red') 
-ax1.tick_params(axis ='y', labelcolor = 'red') 
-  
-# Adding Twin Axes
-ax2 = ax1.twinx()
-ax2.set_ylabel('Voltage between probes [V]', color = 'blue') 
-ax2.plot(plasma_028mbar[0], plasma_028mbar[2], color = 'blue') 
-ax2.tick_params(axis ='y', labelcolor = 'blue')
-plt.savefig("plasma 0.028mbar.png")
-plt.close()
+plt.plot(plasma_0053mbar[0], V_plasma_0053mbar, "b-", label="P = 0.053 mbar")
+plt.xlabel("Current [mA]")
+plt.ylabel("Voltage accros plasma tube [V]")
+plt.plot(plasma_028mbar[0], V_plasma_028mbar, "r-", label="P = 0.28 mbar")
+plt.legend()
+plt.savefig("plasma_voltage_tube.png")
+plt.show()
+
+plt.plot(plasma_028mbar[0], plasma_028mbar[2], "b-", label="P = 0.28 mbar")
+plt.plot(plasma_0053mbar[0], plasma_0053mbar[2], "r-", label = "P = 0.053 mbar")
+plt.xlabel("Current [mA]")
+plt.ylabel("Voltage between probes [V]")
+plt.legend()
+plt.savefig("plasma_voltage_probes.png")
+plt.show()
