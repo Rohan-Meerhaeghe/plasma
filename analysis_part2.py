38eb49e41edcda9c57c01eec47724850d3085ee8
Rohan <rohan.meerhaeghe@ugent.be>
2023-11-15 13:56:19 +0100

    add tangent at origin in analysis_part2.py

--- analysis_part2.py
+++ analysis_part2.py
@@ -8,7 +8,10 @@ class Data():
     def __init__(self, data, pressure, current):
         self.data = data
         self.pressure = pressure
-        self.current = current
+        if (current.endswith(".txt")):
+            self.current = current[:-4]
+        else:
+            self.current = current
 
 def floating_potential(voltage, current):
     zero_index = np.argmin(np.abs(current))
@@ -35,24 +38,27 @@ for filename in os.listdir(directory):
 def asymptote(t, a, b):
     return a * t + b
 
-def fit_asymptotes(xdata,ydata,name,first_a_guess=1,first_b_guess=-2,second_a_guess=1,second_b_guess=2):
+def fit_asymptotes(xdata,ydata,pressure,current,first_a_guess=1,first_b_guess=-2,second_a_guess=1,second_b_guess=2):
     zero_index = np.argmin(np.abs(xdata))
     
     stop_index_0 = round(zero_index/2)
-    popt_0, pcov_0 = curve_fit(asymptote, xdata[:stop_index_0:], ydata[:stop_index_0:], [first_a_guess,first_b_guess])
+    popt_0, pcov_0 = curve_fit(asymptote, xdata[:stop_index_0], ydata[:stop_index_0], [first_a_guess,first_b_guess])
 
     start_index_1 = round((len(xdata)-zero_index)/2+zero_index)
-    popt_1, pcov_1 = curve_fit(asymptote, xdata[start_index_1::], ydata[start_index_1::], [second_a_guess,second_b_guess])
+    popt_1, pcov_1 = curve_fit(asymptote, xdata[start_index_1:], ydata[start_index_1:], [second_a_guess,second_b_guess])
 
-
+    start_index_2 = zero_index - 10
+    stop_index_2 = zero_index + 10
+    popt_2, pcov_2 = curve_fit(asymptote, xdata[start_index_2:stop_index_2],ydata[start_index_2:stop_index_2],[1,0])
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
 
     plt.plot(xdata, ydata, 'b-',label='Data')
     plt.xlabel('Probe voltage [V]',loc='right')
     plt.ylabel('Probe current [mA]',loc='top')
-    plt.plot(xdata[:zero_index:], asymptote(xdata[:zero_index:], *popt_0), 'r.', label='asymptote I2+')
-    plt.plot(xdata[zero_index::], asymptote(xdata[zero_index::], *popt_1), 'g.', label='asymptote I1+')
+    plt.plot(xdata[:zero_index], asymptote(xdata[:zero_index], *popt_0), 'r--', label='asymptote I2+')
+    plt.plot(xdata[zero_index:], asymptote(xdata[zero_index:], *popt_1), 'g--', label='asymptote I1+')
+    plt.plot(xdata[start_index_2:stop_index_2], asymptote(xdata[start_index_2:stop_index_2], *popt_2), 'm--', label='tangent at origin')
 
     plt.legend()
 
@@ -67,10 +73,14 @@ def fit_asymptotes(xdata,ydata,name,first_a_guess=1,first_b_guess=-2,second_a_gu
     # Show ticks in the left and lower axes only
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
-    plt.savefig(name)
+    plt.savefig("langmuir_probe_p_" + pressure + "_i_" + current + ".png")
     plt.close()
+    print("\tPressure: ", pressure, " mbar, current: ", current," mA")
     print("I1+: ",popt_1[1])
+    print("dI1+/dV: ", popt_1[0])
     print("I2+: ", popt_0[1])
+    print("dI2+/dV: ", popt_0[0])
+    print("dI/dV: ", popt_2[0])
 
 for data_set in data:
-    fit_asymptotes(data_set.data[0],data_set.data[1],"langmuir_probe_p_" + data_set.pressure + "_i_" +data_set.current + ".png")
+    fit_asymptotes(data_set.data[0], data_set.data[1], data_set.pressure, data_set.current)
